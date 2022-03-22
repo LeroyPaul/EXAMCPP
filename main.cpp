7b1f22e73910bc264b507cc1e6d31147f24a4f8a
LeroyPaul <93644986+LeroyPaul@users.noreply.github.com>
2022-03-22 11:40:24 +0100

    exercice 3

--- main.cpp
+++ main.cpp
@@ -8,12 +8,20 @@ using namespace std;
 
 int main(){
 
-Lieu * foret = new Lieu;
+Lieu * foret = new Lieu("Foret","On y croise beaucoup d'arbres",8);
+Lieu * montagne = new Lieu("Montagne","On y croise beaucoup de caillous",12);
+Lieu * lac = new Lieu("Lac","On y croise beaucoup d'eau",3);
 
 cout << foret->getName()  << endl;
 cout << foret->getDescription() <<endl;
 cout << foret->getDifficulte() <<endl;
 
+cout << montagne->getName()  << endl;
+cout << montagne->getDescription() <<endl;
+cout << montagne->getDifficulte() <<endl;
 
+cout << lac->getName()  << endl;
+cout << lac->getDescription() <<endl;
+cout << lac->getDifficulte() <<endl;
 
 };
