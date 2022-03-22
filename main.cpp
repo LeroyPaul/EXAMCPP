7a4fe072a7806f1d44684a8f37adeef6cdd36b09
LeroyPaul <93644986+LeroyPaul@users.noreply.github.com>
2022-03-22 10:22:56 +0100

    exercie 1

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+int main(){
+int choix;
+cout << "choisis une destination : 1) foret 2) montagne" << endl;
+cin >> choix;
+if (choix == 1){
+    cout << "la foret a été atteinte";
+}
+if (choix == 2){
+    cout << "la montagne a été atteinte";
+}
+};
