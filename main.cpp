722a8aa708434f374a7aa372fb72664e520dc041
LeroyPaul <93644986+LeroyPaul@users.noreply.github.com>
2022-03-22 11:48:00 +0100

    exercice 3 entier

--- main.cpp
+++ main.cpp
@@ -12,16 +12,30 @@ Lieu * foret = new Lieu("Foret","On y croise beaucoup d'arbres",8);
 Lieu * montagne = new Lieu("Montagne","On y croise beaucoup de caillous",12);
 Lieu * lac = new Lieu("Lac","On y croise beaucoup d'eau",3);
 
-cout << foret->getName()  << endl;
-cout << foret->getDescription() <<endl;
-cout << foret->getDifficulte() <<endl;
 
-cout << montagne->getName()  << endl;
-cout << montagne->getDescription() <<endl;
-cout << montagne->getDifficulte() <<endl;
 
-cout << lac->getName()  << endl;
-cout << lac->getDescription() <<endl;
-cout << lac->getDifficulte() <<endl;
+string choix;
 
+cout << "choisis une destination : foret , montagne ou lac" << endl;
+cin >> choix;
+
+if (choix == "foret"){
+    cout << foret->getName()  << endl;
+    cout << foret->getDescription() <<endl;
+    cout << foret->getDifficulte() <<endl;
+}
+
+
+if (choix == "montagne"){
+    cout << montagne->getName()  << endl;
+    cout << montagne->getDescription() <<endl;
+    cout << montagne->getDifficulte() <<endl;
+}
+
+
+if (choix == "lac"){
+    cout << lac->getName()  << endl;
+    cout << lac->getDescription() <<endl;
+    cout << lac->getDifficulte() <<endl;
+}
 };
